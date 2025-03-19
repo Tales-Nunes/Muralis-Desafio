ac435a0b9fb8a77957e0b0960195af7d0bdf2958
Tales-Nunes <talesnunes.siqueira@gmail.com>
2025-03-19 18:14:10 -0300

    Project open! spring init

---
+++ src/main/java/com/tales/Muralis_Desafio/MuralisDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.tales.Muralis_Desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralisDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuralisDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tales/Muralis_Desafio/MuralisDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tales.Muralis_Desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralisDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
